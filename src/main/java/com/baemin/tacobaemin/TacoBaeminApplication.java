251a2e727b168a0f2ae9a5569dddf5534a8af6f9
takhoyeong <xkrghdud123@naver.com>
2023-09-04 15:57:17 +0900

    start TacoBaemin

---
+++ src/main/java/com/baemin/tacobaemin/TacoBaeminApplication.java
@@ -0,0 +1,13 @@
+package com.baemin.tacobaemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoBaeminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoBaeminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baemin/tacobaemin/TacoBaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baemin.tacobaemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoBaeminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
