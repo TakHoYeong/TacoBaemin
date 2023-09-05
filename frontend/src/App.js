3c2a46f66db8fe4c9af722b6c737863a0a4d0827
takhoyeong <xkrghdud123@naver.com>
2023-09-05 16:23:48 +0900

    <div> 내용 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
 
 function App() {
   const [hello, setHello] = useState("")
 
   useEffect(() => {
-    axios.get('/api/hello')
+    axios.get('/hello')
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
   }, []);
 
   return (
-    <div>
-            백엔드에서 가져온 데이터입니다 : {}
+    <div className='hello'>
+
     </div>
   );
 }
